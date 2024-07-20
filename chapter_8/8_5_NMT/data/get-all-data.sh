5024bc37ac9957a59bc74ab8aa363d15569ef878
Haesun Park <roadhome@gmail.com>
2024-07-20 14:22:57 +0900

    다운로드 경로 수정

--- chapter_8/8_5_NMT/data/get-all-data.sh
+++ chapter_8/8_5_NMT/data/get-all-data.sh
@@ -7,8 +7,8 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p $HERE/nmt
 if [ ! -f $HERE/nmt/eng-fra.txt ]; then 
-    python download.py 1o2ac0EliUod63sYUdpow_Dh-OqS3hF5Z $HERE/nmt/eng-fra.txt # 292
+    python download.py 1LeeoIm9Sn3qAjtzuwo9kDP-Z26ezBKxc $HERE/nmt/eng-fra.txt # 292
 fi 
 if [ ! -f $HERE/nmt/simplest_eng_fra.csv ]; then 
-    python download.py 1jLx6dZllBQ3LXZkCjZ4VciMQkZUInU10 $HERE/nmt/simplest_eng_fra.csv # 30
+    python download.py 1dbqJztanN0PdoMfx8QO8Trg4Z1rXWDty $HERE/nmt/simplest_eng_fra.csv # 30
 fi 
